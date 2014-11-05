dc1f50929c7749cf41de9311d35a90931bde6947
jjpan <fychinesepjj@outlook.com>
2014-11-05 23:40:44 +0800

    1. mysql db connector finished

--- core/db/mydb.py
+++ core/db/mydb.py
@@ -15,6 +15,10 @@ class MultiColumnsError(DBError):
     pass
 
 
+class ColumnTypeError(DBError):
+    pass
+
+
 class Dict(dict):
     '''
     Simple dict but support access by x.y style
@@ -173,6 +177,146 @@ def with_connection(func):
     return _wrapper
 
 
+class _TransactionCtx(object):
+    '''
+    _TransactionCtx object that can handle transactions
+    with _TransactionCtx():
+        pass
+    '''
+    def __enter__(self):
+        global _db_ctx
+        self.should_close_conn = False
+        if not _db_ctx.is_init():
+            _db_ctx.Init()
+            self.should_close_conn = True
+        _db_ctx.transactions = _db_ctx.transactions + 1
+        logging.info('begin transaction...' if _db_ctx.transactions == 1 else 'join current transaction...')
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        global _db_ctx
+        _db_ctx.transactions = _db_ctx.transactions - 1
+        try:
+            if _db_ctx.transactions == 0:
+                if exc_type is None:
+                    self.commit()
+                else:
+                    self.rollback()
+        finally:
+            if self.should_close_conn:
+                _db_ctx.cleanup()
+
+        def commit(self):
+            global _db_ctx
+            logging.info('commit transaction...')
+            try:
+                _db_ctx.connection.commit()
+                logging.info('commit ok.')
+            except:
+                logging.warning('commit failed. try rollback...')
+                _db_ctx.connection.rollback()
+                logging.warning('rollback ok.')
+                raise
+
+        def rollback(self):
+            global _db_ctx
+            logging.warning('rollback transaction...')
+            _db_ctx.connection.rollback()
+            logging.info('rollback ok.')
+
+
+def transaction():
+    '''
+    Create a transaction object, so can use with statement:
+    with transaction():
+        pass
+    '''
+    return _TransactionCtx()
+
+
+def with_transaction(func):
+    @functools.wraps(func)
+    def _wrapper(*args, **kwargs):
+        _start = time.time()
+        with _TransactionCtx():
+            return func(*args, **kwargs)
+        _profiling(_start)
+    return _wrapper
+
+
+def _select(sql, first, *args):
+    'execute select SQL and return unique result or list results'
+    global _db_ctx
+    cursor = None
+    sql = sql.replace('?', '%s')
+    logging.info('SQL: %s, ARGS: %s' % (sql, args))
+    try:
+        cursor = _db_ctx.connection.cursor()
+        cursor.execute(sql, args)
+        #if select statement, cursor have description attr, or description will be None
+        if cursor.description:
+            names = [x[0] for x in cursor.description]
+        if first:
+            values = cursor.fetchone()
+            if not values:
+                return None
+            return Dict(names, values)
+        return [Dict(names, x) for x in cursor.fetchall()]
+    finally:
+        if cursor:
+            cursor.close()
+
+
+@with_connection
+def select_one(sql, *args):
+    return _select(sql, True, *args)
+
+
+@with_connection
+def select_int(sql, *args):
+    d = _select(sql, True, *args)
+    if len(d) != 1:
+        raise MultiColumnsError('Expect only one column')
+    v = d.values()[0]
+    if not isinstance(v, int):
+        raise ColumnTypeError('Expect only integer column')
+    return d.values()[0]
+
+
+@with_connection
+def select(sql, *args):
+    return _select(sql, False, *args)
+
+
+@with_connection
+def _update(sql, *args):
+    global _db_ctx
+    cursor = None
+    sql = sql.replace('?', '%s')
+    logging.info('SQL: %s, ARGS: %s' % (sql, args))
+    try:
+            cursor = _db_ctx.connection.cursor()
+            cursor.execute(sql, args)
+            r = cursor.rowcount
+            if _db_ctx.transaction == 0:
+                logging.info('auto commit')
+                _db_ctx.connection.commit()
+            return r
+    finally:
+        if cursor:
+            cursor.close()
+
+
+def insert(table, **kwargs):
+    cols, args = zip(*kwargs.iteritems())
+    sql = 'insert into `%s` (%s) values (%s)' % (table, ','.join(['`%s`' % col for col in cols]), ','.join(['?' for i in range(len(cols))]))
+    return _update(sql, *args)
+
+
+def update(sql, *args):
+    return _update(sql, *args)
+
+
 def mysql_engine(user, password, database, host='127.0.0.1', port=3306, **kwargs):
     import MySQLdb
     params = dict(user=user, passwd=password, db=database, host=host, port=port)
@@ -196,5 +340,4 @@ if __name__ == '__main__':
     #import doctest
     #doctest.testmod()
     create_engine(mysql_engine, 'root', '123456', 'frame-test')
-    conn = engine.connect()
-    cursor = conn.cursor()
+    print select_int('select name from test')
