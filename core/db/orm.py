5d0a463379dff05be2ded4ed8f7ba38dfec1af2f
jjpan <jjpan@bainainfo.com>
2014-11-07 18:57:25 +0800

    finished orm module

--- core/db/orm.py
+++ core/db/orm.py
@@ -162,6 +162,106 @@ class ModelMetaClass(type):
                 attrs['trigger'] = None
         return type.__new__(cls, name, bases, attrs)
 
+
+class Model(dict):
+    __metaclass__ = ModelMetaClass
+
+    def __init__(self, **kwargs):
+        super(Model, self).__init__(**kwargs)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    @classmethod
+    def get(cls, pk):
+        '''
+        Get by primary key
+        '''
+        d = db.select_one('select * from %s where %s=?' % (cls.__table__, cls.__primary_key__.name), pk)
+        return cls(**d) if d else None
+
+    @classmethod
+    def find_one(cls, where, *args):
+        '''
+        Find by where clause and return one result. If multiple results found,
+        only the first one returned. If no results found, return None
+        '''
+        d = db.select_one('select * from `%s` %s' % (cls.__table__, where), *args)
+        return cls(**d) if d else None
+
+    @classmethod
+    def find_all(cls, *args):
+        '''
+        Find all and return list.
+        '''
+        L = db.select('select * from `%s`' % cls.__table__)
+        return [cls(**d) for d in L]
+
+    @classmethod
+    def find_by(cls, where, *args):
+        '''
+        Find by where clause and return list
+        '''
+        L = db.select('select * from `%s` %s' % (cls.__table__, where), *args)
+        return [cls(**d) for d in L]
+
+    @classmethod
+    def count_all(cls):
+        '''
+        Find by 'select count(pk) from table' and return integer.
+        '''
+        return db.select_int('select count(`%s`) from `%s`' % (cls.__primary_key__.name, cls.__table__))
+
+    @classmethod
+    def count_by(cls, where, *args):
+        '''
+        Find by 'select count(pk) from table where...' and return int
+        '''
+        return db.select_int('select count(`%s`) from `$s` %s' % (cls.__primary_key__.name, cls.__table__, where), *args)
+
+    def update(self):
+        self.pre_update and self.pre_update()
+        L = []
+        args = []
+        for k, v in self.__mapping__.iteritems():
+            if v.updatable:
+                if hasattr(self, k):
+                    arg = getattr(self, k)
+                else:
+                    arg = v.default
+                    setattr(self, k, arg)
+                L.append('`%s`=?' % k)
+                args.append(arg)
+        pk = self.__primary_key__.name
+        args.append(getattr(self, pk))
+        db.update('update `%s` set %s where %s=?' % (self.__table__, ','.join(L), pk), *args)
+        return self
+
+    def delete(self):
+        self.pre_delete and self.pre_delete()
+        pk = self.__primary_key__.name
+        args = (getattr(self, pk), )
+        db.update('delete from `%s` where `%s`=?' % (self.__table__, pk), *args)
+        return self
+
+    def insert(self):
+        self.pre_insert and self.pre_insert()
+        params = {}
+        for k, v in self.__mapping__.iteritems():
+            if v.insertable:
+                if not hasattr(self, k):
+                    setattr(self, k, v.default)
+                params[v.name] = getattr(self, k)
+        db.insert('%s' % self.__table__, **params)
+        return self
+
+
 if __name__ == '__main__':
     f = Field(name='abc')
     print f
